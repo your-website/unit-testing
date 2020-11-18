7f3627dc7229ca2fcf4d2213ca64bb2a49996ae6
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-18 19:36:27 +0700

    testing props & user interaction

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>
+  <div class="app-header">
     <button v-show="loggedIn">Logout</button>
+    <RandomNumber />
   </div>
 </template>
 
 <script>
+import RandomNumber from '@/components/RandomNumber.vue'
 export default {
+  components: {
+    RandomNumber
+  },
   data() {
     return {
       loggedIn: false
@@ -14,4 +19,13 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.app-header {
+  min-height: 40px;
+  padding: 0.2em 1em;
+  background: white;
+}
+button {
+  float: right;
+}
+</style>
