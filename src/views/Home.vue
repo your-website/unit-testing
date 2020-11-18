2dae670b0a94f44c66879704bb1c2b6dc32a6722
Roman Kolpachev <roman.kolpachev@yandex.ru>
2020-11-18 21:18:51 +0700

    jest emitted events

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="home">
     <AppHeader msg="Welcome to Your Vue.js App" />
+    <LoginForm />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import AppHeader from '@/components/AppHeader.vue'
+import LoginForm from '@/components/LoginForm.vue'
 
 export default {
   name: 'Home',
   components: {
-    AppHeader
+    AppHeader,
+    LoginForm
   }
 }
 </script>
